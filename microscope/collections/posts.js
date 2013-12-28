a15064322f17821b4aa1d01da975303d3d28d073
Peter Bang <pbang@college.harvard.edu>
2013-12-28 09:20:25 +0900

    fix typos

--- microscope/collections/posts.js
+++ microscope/collections/posts.js
@@ -30,7 +30,7 @@ Meteor.methods({
 		}
 
 		// pick out the whitelisted keys
-		var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'){
+		var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
 			userId: user._id,
 			author: user.username,
 			submitted: new Date().getTime()
