dac36ef14ee1d0edea6748b75481f39ebd88eb85
Peter Bang <pbang@college.harvard.edu>
2013-12-30 09:49:00 +0900

    added basic permission to check the post's owner'

--- microscope/collections/posts.js
+++ microscope/collections/posts.js
@@ -1,13 +1,11 @@
 Posts = new Meteor.Collection('posts');
 
-/*
+
 Posts.allow({
-	insert: function(userId, doc) {
-		// only allow posting if you are logged in
-		return !! userId;
-	}
+	update: ownsDocument,
+	remove: ownsDocument
 });
-*/
+
 
 Meteor.methods({
 	post: function(postAttributes) {
